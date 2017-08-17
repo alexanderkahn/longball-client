20a42a6a40fe9fd0d51065bd837d43a139475032
Alexander Kahn <akahn@jamasoftware.com>
2017-08-17 16:26:53 -0700

    master:  A few cosmetic changes just to get my feet wet

--- src/App.js
+++ src/App.js
@@ -2,20 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class LongballApp extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="longball-app">
+        <div className="app-header">
+          <img src={logo} className="app-logo" alt="logo" />
+          <h2 className="app-title">Longball</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
 }
 
-export default App;
+export default LongballApp;
