8dd017fbcf87c9eefc544de3daa4e82b3404e141
Alexander Kahn <alexanderkahn@users.noreply.github.com>
2017-08-17 23:05:31 -0700

    master:  Add the bones of a login link. Spooooky

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class LongballApp extends Component {
-  render() {
+    render() {
+        return (
+            <Header />
+        );
+    }
+}
+
+function Header() {
     return (
-      <div className="longball-app">
-        <div className="app-header">
-          <img src={logo} className="app-logo" alt="logo" />
-          <h2 className="app-title">Longball</h2>
+        <div className="longball-app">
+            <div className="app-header">
+                <img src={logo} className="app-logo" alt="logo"/>
+                <h2 className="app-title">Longball</h2>
+                <User />
+            </div>
         </div>
-      </div>
     );
-  }
+}
+
+function User() {
+    const isLoggedIn = true;
+    if (isLoggedIn) {
+        return <AuthenticatedUser />
+    } else {
+        return <AnonymousUser />
+    }
+}
+
+function AuthenticatedUser() {
+    return <span className="header-user">Hey, dude!</span>
+}
+
+function AnonymousUser() {
+    return <span classID="header-user"><a href="">Sign in</a></span>
 }
 
 export default LongballApp;
